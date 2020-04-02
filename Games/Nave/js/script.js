afef82baddad8854e48f35f614e7712bc139631a
Pedro <xpedrostewart@gmail.com>
2020-04-02 01:39:07 -0300

    Tela toda. Movimento fluido de nave. Dois tipos de obstáculos. Tiros mais lentos. Obstáculos surgem horizontalmente.

--- Games/Nave/js/script.js
+++ Games/Nave/js/script.js
@@ -1,31 +1,59 @@
-let passo_nave = 25;
+let passo_nave = 5;
 let passo_obstaculos = 1;
-let velocidade_obstaculos = 17; // menos é mais
-let qtde_inicial_obstaculos = 7;
-
-let vel_recarga_laser = 1;
-let passo_laser = 95;
+let velocidade_obstaculos = 10; // menos é mais
+let qtde_inicial_obstaculos = 10;
+let vel_recarga_laser = 10;
+let passo_laser = 25;
 let pode_atirar = true;
-let limite_tiro = 5;
-
+let limite_tiro = 25;
 let valor_retorno = 5;
 let repelencia = 100;
 let parcela_correcao = 5;
-
-let min_top_aleatorio = 400;
-let max_top_aleatorio = 600;
-
+let min_left_aleatorio = -100;
+let max_left_aleatorio = 0;
 let obst_fim = 0;
 let obst_dest = 0;
-
 let tiros_dados = 0;
 let tiros_errados = 0;
-
-let quebra = false;
-
+let fim_jogo = false;
 let qtde_movimento = 20;
-
 let qtde_maxima_obstaculos = 25;
+let pontuacao = 0;
+
+let acoes_nave = {
+	'moveu_esquerda':false,
+	'moveu_direita':false,
+	'atirou':false
+};
+
+// ALTERNATIVAS
+
+let qtde_alternativas = 8;
+let binario = '';
+let numeros = [];
+let x = 128;
+
+numeros.push(x);
+for (let i = 0; i < qtde_alternativas; i++) {
+
+	if (x / 2 >= 1) {
+		numeros.push(x = x / 2);
+	}
+
+}
+
+for (let i = 0; i < qtde_alternativas; i++) {
+
+	let r = aleatorio(0, 1);
+	binario += r;
+	let $a = $(`<div class="alt"><span class="ajuda">${(numeros[i])}</span><span class="valor_binario">${r}</span></div>`);
+	$('#alternativas').append($a);
+
+}
+
+binario = parseInt(binario, 2);
+let decimal = parseInt(binario, 10);
+$('#numero_atual').text(decimal);
 
 function aleatorio(min, max) {
 
@@ -43,192 +71,267 @@ function cria_obstaculos(quantidade) {
 
 	for (let i = 0; i < quantidade; i++) {
 
-		let $obstaculo = $(`<div class="bloco obstaculo"></div>`);
-		$($obstaculo).css('position', 'absolute');
+		let $obstaculo = $(`<div class="obstaculo"></div>`);
+		// $($obstaculo).css('position', 'absolute');
 
-		let top = aleatorio(min_top_aleatorio, max_top_aleatorio);
-		let left = aleatorio(0, $('#espaco_obstaculos').width() - 50);
+		let left = aleatorio(min_left_aleatorio, max_left_aleatorio);
+		let top = aleatorio(0, 50);
 
-		let r = aleatorio(0, 1);
+		let r = aleatorio(0, 3);
 
 		if (r == 0) {
 			$($obstaculo).css('border-radius', '50%');
 			$($obstaculo).css('background-image', `linear-gradient(#${aleatorio(100, 999)}, #${aleatorio(100, 999)})`);
-		} else {
+			$obstaculo.addClass('planeta');
+		} else if (r == 1) {
 			$($obstaculo).css('width', '100px');
 			$($obstaculo).css('height', '100px');
 			$($obstaculo).css('border-radius', '50%');
 			$($obstaculo).css('background-image', `linear-gradient(#${aleatorio(100, 999)}, #${aleatorio(100, 999)})`);
+			$obstaculo.addClass('planeta');
+		} else {
+			$($obstaculo).css('background-image', `url('img/alien.png')`);
+			$obstaculo.addClass('alien');
 		}
 
-		$($obstaculo).css('top', `${top}px`);
-		$($obstaculo).css('left', `${left}px`);
-		$('#espaco_obstaculos').append($obstaculo);
+		$($obstaculo).css('top', `${top}%`);
+		$($obstaculo).css('left', `${left}%`);
+		$('#espaco').append($obstaculo);
 
 	}
 
 }
 
-function move_obstaculos () {
+cria_obstaculos(qtde_inicial_obstaculos);
 
-	// Esta função simplesmente incrementa o top cada um dos obstáculos. Ela fica dentro de um intervalo, que está logo abaixo.
+window.addEventListener('resize', function() {
+	$('#espaco').css('width', `${window.innerWidth}px`);
+	$('#espaco').css('height', `${window.innerHeight}px`);
+});
 
-	if ($('#espaco_obstaculos').offset().top < 1250) {
-		$('#espaco_obstaculos').css('top', `+=${passo_obstaculos}`);
-	}
+let y = false;
+
+function atualiza () {
+
+	verifica_formacao_binaria();
+	verifica_limite_espaco();
+    
+    // Aqui as ondas se iniciam
+    if ($('.planeta').length == 0 && $('.alien').length == 0 && !fim_jogo) {
+    	$('.obstaculo').css('left', '-100%');
+    	cria_obstaculos(qtde_inicial_obstaculos);
+    	qtde_inicial_obstaculos++;
+    }
+
+	$('.obstaculo').css('left', `+=${passo_obstaculos}`);
 
 	for (let i = 0; i < $('.obstaculo').length; i++) {
 
-		// É preciso chamar a função que detecta a colisão para os obstáculos também
-		detecta_colisao($('.obstaculo').eq(i));
-
 		// Se o obstáculo chegou ao fim do espaço, ele fica escondido e recebe uma classe de chegou ao fim
-		if ($('.obstaculo').eq(i).offset().top > $('#espaco').height() - $('.obstaculo').eq(i).width()) {
+		if ($('.obstaculo').eq(i).offset().left > $('#espaco').width() - $('.obstaculo').eq(i).width()) {
 			$('.obstaculo').eq(i).remove();
 			obst_fim++
+			console.log('removi');
 		}
 	}
 
-	let intervalo_func = setTimeout(move_obstaculos, velocidade_obstaculos);
+	if (acoes_nave['moveu_direita']) {
 
-	if (quebra) {
-		clearTimeout(intervalo_func);
-	}
-}
+		acoes_nave['moveu_direita'] = false;
+		let cont = 0;
+		function move_direita() {
 
-let colisoes = 0;
-move_obstaculos();
+			$('#nave').css('left', '+=1px');
 
-function detecta_colisao($bloco) {
+			let id_parada = requestAnimationFrame(move_direita);
 
-	/*
-		Esta função detecta a colisão da nave com os blocos. Essa colisão pode ser acima, à esquerda, abaixo ou à direita. Para isso, pega-se o top e o left de cada bloco, bem como o top e left da nave. Além disso, são necessários o width e o height de cada um desses elementos. Baseando-se em limiares, fica mais simples detectar a colisão. Com esses limiares, define-se o espaço crítico, onde a colisão deve acontecer. E a colisão, por sua vez, é na verdade uma repulsão. Uma vez que a nave entra no espaço crítico, ela é repilida, diminuindo-se ou aumentando-se seu top ou seu left. O "nível de repulsão" é dado por uma constante, que pode ser alterada no início do código.
-	*/
-
-	let top_bloco = $bloco.offset().top;
-	let left_bloco = $bloco.offset().left;
-
-	let height_bloco = $bloco.height();
-	let width_bloco = $bloco.width();
-
-	let top_nave = $('#nave').offset().top;
-	let left_nave = $('#nave').offset().left;
-	
-	let height_nave = $('#nave').height();
-	let width_nave = $('#nave').width();
-
-	// ESQUERDA
-	let limiar_esquerdo = left_bloco - width_bloco; // não pode ser maior do que isso
-	// CIMA
-	let limiar_superior = top_bloco - height_bloco; // não pode ser menor do que isso
-	// BAIXO
-	let limiar_inferior = top_bloco; // não pode passar disso
-	// DIREITA
-	let limiar_direito = left_bloco; // não pode ser menor do que isso;
-
-	function dano() {
-		$('#nave').css('box-shadow', '1px 1px 50px tomato');
-
-		setTimeout(() => {
-			$('#nave').css('box-shadow', 'none');
-		}, 300);
-
-	}
-
-	if (left_nave >= limiar_esquerdo && left_nave < limiar_esquerdo + parcela_correcao) {
-		if (top_nave < limiar_inferior + width_nave) {
-			if (top_nave > limiar_superior) {
-				//console.log('você tocou a esquerda do bloco');
-				$('#nave').css('left', `-=${repelencia}px`);
-				dano();
-				colisoes++;
+			if (cont == 15) {
+				cancelAnimationFrame(id_parada);
 			}
+
+			cont++;
+
 		}
+
+		move_direita();
+
 	}
 
-	if (left_nave >= limiar_direito && left_nave < limiar_direito + width_bloco + parcela_correcao) {
-		if (top_nave < limiar_inferior + width_nave) {
-			if (top_nave > limiar_superior) {
-				//console.log('você tocou a direita do bloco');
-				$('#nave').css('left', `+=${repelencia}px`);
-				dano();
-				colisoes++;
+	if (acoes_nave['moveu_esquerda']) {
+		
+		acoes_nave['moveu_esquerda'] = false;
+		let cont = 0;
+		function move_esquerda() {
+
+			$('#nave').css('left', '-=1px');
+
+			let id_parada = requestAnimationFrame(move_esquerda);
+
+			if (cont == 15) {
+				cancelAnimationFrame(id_parada);
 			}
+
+			cont++;
+
 		}
+
+		move_esquerda();
+
 	}
 
-	if (top_nave >= limiar_inferior && top_nave < limiar_inferior + height_bloco + parcela_correcao) {
-		if (left_nave < limiar_direito + width_nave) {
-			if (left_nave > limiar_esquerdo) {
-				//console.log('você tocou a parte de baixo do bloco');
+	if (acoes_nave['atirou']) {
+		acoes_nave['atirou'] = false;
 
-				if (aleatorio(1, 2) == 1) {
-					$('#nave').css('left', `+=${repelencia}px`);
-				} else {
-					$('#nave').css('left', `-=${repelencia}px`);
+		$('#laser').show();
+
+		let cont = 0;
+		function atira () {
+			
+			pode_atirar = false;
+			$('#laser').css('top', `-=${passo_laser}`);
+
+			for (let i = 0; i < $(`.alt`).length; i++) {
+				if (atira_alt($(`.alt`).eq(i))) {
+					y = true;
+				}
+			}
+
+			if (!y) {
+				for (let i = 0; i < $('.planeta').length; i++) {
+					atira_planeta($('.planeta').eq(i));
 				}
 
-				dano();
-				colisoes++;
+				for (let i = 0; i < $(`.alien`).length; i++) {
+					atira_alien($(`.alien`).eq(i));
+				}
 			}
+
+			let tiro = setTimeout(atira, vel_recarga_laser);
+
+			if (cont == limite_tiro) {
+
+				clearTimeout(tiro);
+
+				$('#laser').hide();
+				$('#laser').css('top', '0px');
+				
+				pode_atirar = true;
+			}
+
+			cont++;
+
+		}
+
+		if (pode_atirar) {
+			atira();
 		}
 	}
 
+	let intervalo_func = setTimeout(atualiza, velocidade_obstaculos);
 }
 
-function verifica_tiro($bloco) {
+atualiza();
 
-	// Funciona exatamente igual à função de detecção de colisão. Se o "laser", que nada mais é do que uma div cujo top é modificado para simular movimento, entra na região crítica de um obstáculo, o obstáculo é escondido e recebe a classe "obstaculo_destruido", para ser apagado no final.
+function atira_alt(alternativa) {
 
 	let top_laser = $('#laser').offset().top;
 	let left_laser = $('#laser').offset().left;
 
-	let height_laser = $('#laser').height();
+	let top_alternativa = $(alternativa).offset().top;
+	let left_alternativa = $(alternativa).offset().left;
 
-	let top_bloco = $bloco.offset().top;
-	let left_bloco = $bloco.offset().left;
+	let height_alternativa = $(alternativa).height();
+	let width_alternativa = $(alternativa).width();
 
-	let height_bloco = $bloco.height();
-	let width_bloco = $bloco.width();
-
-	// O top do laser diminui progressivamente, porque está subindo
-	// O top do bloco aumenta progressivamente, porque está descendo
-
-	// Se o top do bloco é 500, o top do laser tem que ser menor do que isso, para ultrapassar o bloco. Porém, o top do laser precisa ser maior do que o top do bloco menos o height do bloco.
-
-	if (top_laser < top_bloco && top_laser > top_bloco - height_bloco * 2) {
-		if (left_laser > left_bloco && left_laser < left_bloco + width_bloco) {
-
-			if ($bloco.hasClass('alt')) {
-
-				$bloco.css('box-shadow', '1px 1px 50px lightgreen');
-
-				if ($bloco.find('.valor_binario').text() == '1') {
-					$bloco.find('.valor_binario').text('0');
-				} else if ($bloco.find('.valor_binario').text() == '0') {
-					$bloco.find('.valor_binario').text('1');
-				}
-
-				setTimeout(() => {
-					$bloco.css('box-shadow', 'none');
-				}, 250);
-
-				verifica_formacao_binaria();
-
-			} else {
-				$bloco.css('background-image', 'linear-gradient(red, tomato)').css('box-shadow', '1px 1px 50px orange');
-				setTimeout(() => {
-					$bloco.fadeOut('600');
-				}, 25);
-
-				setTimeout(() => {
-					$bloco.remove();
-				}, 500);			
-			}
+	if (top_laser < top_alternativa && top_laser > top_alternativa - height_alternativa) {
+		if (left_laser > left_alternativa && left_laser < left_alternativa + width_alternativa) {
 
 			$('#laser').hide();
 			$('#laser').css('top', '0px');
 
+			$(alternativa).css('box-shadow', '1px 1px 50px lightgreen');
+
+			if ($(alternativa).find('.valor_binario').text() == '1') {
+				$(alternativa).find('.valor_binario').text('0');
+			} else if ($(alternativa).find('.valor_binario').text() == '0') {
+				$(alternativa).find('.valor_binario').text('1');
+			}
+
+			setTimeout(() => {
+				$(alternativa).css('box-shadow', 'none');
+				return true;
+			}, 250);
+		}
+	}
+
+}
+
+function atira_planeta(planeta) {
+
+	// Se o "laser", que nada mais é do que uma div cujo top é modificado para simular movimento, entra na região crítica de um obstáculo, o obstáculo é escondido e recebe a classe "obstaculo_destruido", para ser apagado no final.
+
+	let top_laser = $('#laser').offset().top;
+	let left_laser = $('#laser').offset().left;
+
+	let top_planeta = $(planeta).offset().top;
+	let left_planeta = $(planeta).offset().left;
+
+	let height_planeta = $(planeta).height();
+	let width_planeta = $(planeta).width();
+
+	// O top do laser diminui progressivamente, porque está subindo
+	// O top do bloco aumenta progressivamente, porque está descendo
+
+	// Se o top do bloco é 500, o top do laser tem que ser menor do que isso, para ultrapassar o bloco. Porém, o top do laser precisa ser maior do que o top do bloco menos o height do bloco. Ou seja, precisa ultrapassar a parte de baixo do bloco.
+
+	if (top_laser > 0 && left_laser > 0) {
+		if (top_laser < top_planeta && top_laser > top_planeta - height_planeta) {
+			if (left_laser > left_planeta && left_laser < left_planeta + width_planeta) {
+
+				$('#laser').hide();
+				$('#laser').css('top', '1px');	
+				$(planeta).css('box-shadow', '1px 1px 50px tomato');
+				setTimeout(() => {
+					$(planeta).css('box-shadow', 'none')
+				}, 250);
+
+
+			}
+		}
+	}
+
+}
+
+function atira_alien(alien) {
+
+	let top_laser = $('#laser').offset().top;
+	let left_laser = $('#laser').offset().left;
+
+	let top_alien = $(alien).offset().top;
+	let left_alien = $(alien).offset().left;
+
+	let height_alien = $(alien).height();
+	let width_alien = $(alien).width();
+
+	if (top_laser > 0 && left_laser > 0) {
+		if (top_laser < top_alien && top_laser > top_alien - height_alien) {
+			if (left_laser > left_alien && left_laser < left_alien + width_alien) {
+
+				$('#laser').hide();
+				$('#laser').css('top', '0px');
+
+				$(alien).css('background-image', 'linear-gradient(red, tomato)').css('box-shadow', '1px 1px 50px orange');
+				setTimeout(() => {
+					$(alien).fadeOut('600');
+				}, 25);
+
+				setTimeout(() => {
+					$(alien).remove();
+					pontuacao++;
+					$('#pontuacao').text(pontuacao);
+				}, 500);	
+
+			}
 		}
 	}
 
@@ -272,19 +375,9 @@ function verifica_limite_espaco() {
 	let height_nave = $('#nave').height();
 	let width_nave = $('#nave').width();
 
-	// cima
-	if (top_nave < passo_nave + 2) {
-		$('#nave').css('top', `+=${passo_nave}px`);
-	}
-
-	// baixo
-	if (top_nave > height_espaco - (height_nave / 2)) {
-		$('#nave').css('top', `${height_espaco - height_nave}px`);
-	}
-
 	// direita
-	if (left_nave > width_espaco + left_espaco - width_nave) {
-		$('#nave').css('left', `${0}px`);
+	if (left_nave > width_espaco - 10) {
+		$('#nave').css('left', `${11}px`);
 	}
 
 	// esquerda
@@ -299,9 +392,8 @@ function gameover(msg) {
 	toast(msg, 'brown');
 	$('#bg_modal').fadeIn('2000');
 	$('.obstaculo').remove();
-	$('#espaco_obstaculos').css('top', '-600px');
-	clearTimeout(move_obstaculos);
-	quebra = true;
+	fim_jogo = true;
+	clearTimeout(intervalo_func);
 }
 
 $('#jogar_novamente').click(() => {
@@ -315,9 +407,9 @@ function temporizador() {
   let t = setTimeout(function() {
     
   	if (tempo_restante > 0) {
-  		$('#tempo').text(tempo_restante--).css('color', 'green');
+  		$('#tempo').text(tempo_restante--).css('color', 'black');
   	} else {
-  		$('#tempo').text(tempo_restante--).css('color', 'tomato');
+  		$('#tempo').text(tempo_restante--).css('color', 'brown');
   		toast('Seu tempo acabou.', 'orange');
   	}
 
@@ -330,112 +422,26 @@ function temporizador() {
   
 temporizador();
 
-let tot_destruidos = 0;
-let tot_sem_destruir = 0;
-
-let c = 0;
-let onda = 1;
-
-toast('1ª onda.', 'darkblue');
-cria_obstaculos(qtde_inicial_obstaculos);
-
 // CENTRO DE AÇÕES
 
 $('body').keydown(function(event) {
 
-	// Este evento identifica as setas que foram pressionadas para mover a nave, bem como o espaço que serve para atirar. O uso do objeto "map" serve para identificar as setas que foram pressionadas ao mesmo tempo, a fim de realizar o movimento de diagonal.
-
-	if ($('.obstaculo').length < qtde_inicial_obstaculos) {
-		obst_dest = qtde_inicial_obstaculos - $('.obstaculo').length - obst_fim;
-		$('#pontuacao').text(tot_destruidos + obst_dest);
-	}
-
-
-	$('#colisoes').text(colisoes);
-
-    if (colisoes > 2) {
-    	gameover('Opa, a sua nave quebrou!');
-    	colisoes = 0;
-    	exibiu = false;
-    }
-    
-    // Aqui as ondas se iniciam
-    if ($('.obstaculo').length == 0 && !quebra) {
-
-    	tot_destruidos += obst_dest;
-    	tot_sem_destruir += obst_fim;
-
-    	$('#pontuacao').text(tot_destruidos);
-
-    	obst_fim = 0;
-    	onda++;
-    	exibiu = false;
-    	min_top_aleatorio = 350;
-    	max_top_aleatorio = 600;
-	    $('#espaco_obstaculos').css('top', `-${600}px`);
-
-	    if (qtde_inicial_obstaculos <= 15) {
-	    	qtde_inicial_obstaculos++;
-	    }
-
-
-    	cria_obstaculos(qtde_inicial_obstaculos);
-    	toast(`${onda}ª onda.`, 'darkblue');
-
-    }
-
 	let tecla = event.keyCode;
-	verifica_limite_espaco();
 
 	if(tecla == 39 || tecla == 68) {
-		 // seta pra DIREITA
-		$('#nave').css('left', `+=${passo_nave}`);
-
+		 // seta pra DIREITA ou D
+		 acoes_nave['moveu_direita'] = true;
 	}
 
 	if(tecla == 37 || tecla == 65) {
-		 // seta pra ESQUERDA
-		$('#nave').css('left', `-=${passo_nave}`);
-
+		 // seta pra ESQUERDA ou A
+		 acoes_nave['moveu_esquerda'] = true;
 	}
 
 	if (tecla == 32) {
 		// espaço
 		tiros_dados++;
-
-		$('#laser').show();
-		let cont = 0;
-		function atira () {
-			pode_atirar = false;
-			$('#laser').css('top', `-=${passo_laser}`);
-
-			if ($('.obstaculo').length > 0) {
-				for (let i = 0; i < $('.obstaculo').length; i++) {
-					verifica_tiro($('.obstaculo').eq(i));
-				}
-			}
-
-			for (let i = 0; i < $(`.alt`).length; i++) {
-				verifica_tiro($(`.alt`).eq(i));
-			}
-
-			let tiro = setTimeout(atira, vel_recarga_laser);
-
-			if (cont == limite_tiro) {
-				clearTimeout(tiro);
-				$('#laser').css('top', '-10px');
-				$('#laser').hide();
-				pode_atirar = true;
-			}
-
-			cont++;
-
-		}
-
-		if (pode_atirar) {
-			atira();
-		}
-
+		acoes_nave['atirou'] = true;
 	}
 
 });
@@ -464,35 +470,3 @@ function toast(texto, cor = 'green') {
 $('.fechar').click(function() {
 	$('.toast').fadeOut('500');
 });
-
-// FIM TOAST
-
-
-// ALTERNATIVAS
-
-let qtde_alternativas = 8;
-let binario = '';
-let numeros = [];
-let x = 128;
-
-numeros.push(x);
-for (let i = 0; i < qtde_alternativas; i++) {
-
-	if (x / 2 >= 1) {
-		numeros.push(x = x / 2);
-	}
-
-}
-
-for (let i = 0; i < qtde_alternativas; i++) {
-
-	let r = aleatorio(0, 1);
-	binario += r;
-	let $a = $(`<div class="alt"><span class="ajuda">${(numeros[i])}</span><span class="valor_binario">${r}</span></div>`);
-	$('#alternativas').append($a);
-
-}
-
-binario = parseInt(binario, 2);
-let decimal = parseInt(binario, 10);
-$('#numero_atual').text(decimal);
