d0d1cb60eb6839a0d86ab26587e474a1c1f9ff1f
Marx Linden <marx.linden@ifb.edu.br>
2020-06-18 23:01:31 -0300

    New logo and CSS changes

--- Client/src/app/topbar/topbar.component.ts
+++ Client/src/app/topbar/topbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topbar.component.scss']
 })
 export class TopbarComponent implements OnInit {
+
+  @Input() showLogo: boolean = true;
 
   constructor() { }
 
