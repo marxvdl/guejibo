20c63f6cbc13e76f2ce2783a3e41bdd2694a95ae
iampedrossb <xpedrostewart@gmail.com>
2022-06-13 17:33:19 -0300

    Trecho de código que impede o usuário de inspecionar a página

--- Games/torne-verdadeiro/js/jogo.js
+++ Games/torne-verdadeiro/js/jogo.js
@@ -1926,24 +1926,24 @@ function resetaLocalStorage() {
 resetaLocalStorage();
 
 // impede o usuário de inspecionar o jogo
-// document.addEventListener('contextmenu', e => {
-//     e.preventDefault();
-// });
+document.addEventListener('contextmenu', e => {
+    e.preventDefault();
+});
 
-// document.onkeydown = function(e) {
-//     if (event.keyCode == 123) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-//         return false;
-//     }
-// }
+document.onkeydown = function(e) {
+    if (event.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
